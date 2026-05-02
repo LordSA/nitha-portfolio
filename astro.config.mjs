15c2666ed4d773c4d3ff09e7cc2f48d82673f528
LordSA <dlshacker159@gmail.com>
2026-05-02 12:05:52 +0530

    main page setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    optimizeDeps: {
+      include: ['gsap', 'three', 'lenis'],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            three: ['three'],
+            gsap: ['gsap'],
+          },
+        },
+      },
+    },
+  },
+});
