3788942142790bdedcb40cb8a9e855a1426e0176
LordSA <dlshacker159@gmail.com>
2026-05-02 12:10:32 +0530

    some works

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   vite: {
     optimizeDeps: {
-      include: ['gsap', 'three', 'lenis'],
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            three: ['three'],
-            gsap: ['gsap'],
-          },
-        },
-      },
+      include: ['gsap', 'lenis'],
     },
   },
 });
